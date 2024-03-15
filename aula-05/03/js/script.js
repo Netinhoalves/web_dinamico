b2aa084233c0034d6fe74c2f18232b9ff860e83c
netinhoalves <netinhoalves_2011@hotmail.com>
2024-03-14 20:02:41 -0400

    exercício refeito como professor pediu

--- aula-05/03/js/script.js
+++ aula-05/03/js/script.js
@@ -1,10 +1,5 @@
-function botao() {
-  let btn = document.getElementById("btn");
-  btn.innerText = "Show Less"
-}
-
-let open = document.querySelector('article');
+let open = document.querySelector('article')
 
 function showMore() {
-  open.classLista.toggle('show')
+    open.classList.toggle('open')
 }
